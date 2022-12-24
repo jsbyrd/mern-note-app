f77e15632e59512244c4c0041fade94a1d77eb71
joshua <jsbyrd02@gmail.com>
2022-12-24 16:40:59 -0500

    Updated functionality of changing the importance of a note

--- server/index.js
+++ server/index.js
@@ -7,27 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true
-  }
-];
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 });
@@ -47,9 +26,27 @@ app.get('/api/notes/:id', (request, response) => {
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id);
-  notes = notes.filter(note => note.id !== id);
+  Note.findByIdAndRemove(id)
+    .then((response) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
-  response.status(204).end();
+app.put('/api/notes/:id', (request, response) => {
+  const body = request.body;
+  const id = Number(request.params.id);
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.post('/api/notes', (request, response) => {
